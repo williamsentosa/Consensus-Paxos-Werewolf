4b8d0d9e7c73529afd1448b6df1f04ddea54148c
William Sentosa <williamsentosa.sentosa@gmail.com>
2016-04-26 02:14:34 +0700

    added json library and added client functions

--- WerewolfGame/src/client/Client.java
+++ WerewolfGame/src/client/Client.java
@@ -5,12 +5,14 @@
  */
 package client;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.JSONObject;
 
 /**
  *
@@ -19,14 +21,18 @@ import java.util.logging.Logger;
 public class Client {
     private Socket socket;
     private DataOutputStream out;
+    private DataInputStream in;
     private String ipAddr;
     private int port;
+    private Scanner scanner;
+    private int playerId = -1;
     
     public Client() {
-        // do nothing;
+        scanner = new Scanner(System.in);
     }
     
     public Client(String ipAddr, int port) {
+        scanner = new Scanner(System.in);
         this.connect(ipAddr, port);
     }
     
@@ -36,6 +42,7 @@ public class Client {
         try {
             socket = new Socket(ipAddr, port);   
             out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -49,22 +56,131 @@ public class Client {
         }
     }
     
-    public static void main(String args[]) {
-        Scanner scanner = new Scanner(System.in);
-        String ipAddr = "";
-        int port = 0;
-        System.out.print("Input server ip address : ");
-        ipAddr = scanner.nextLine();
-        System.out.print("Input server port : ");
-        port = scanner.nextInt();
-        Client client = new Client(ipAddr, port);
-        System.out.println("Connected to " + ipAddr + " port " + port);
-        scanner.next();
-        String msg = "";
-        while(true) {
-            msg = scanner.nextLine();
-            client.send(msg);
+    public void run() {
+        String request = "";
+        String command = "";
+        while (true) {
+            System.out.print("Input Command : ");
+            command = scanner.nextLine();
+            switch (command) {
+                case "join game" : joinCommand(); break;
+                case "leave game" : leaveCommand(); break;
+                case "ready up" : readyUpCommand(); break;
+                case "list client" : listClientCommand(); break;
+            }
         }
-        
+    }
+    
+    private void joinCommand() {
+        System.out.print("Input username : ");
+        String username = scanner.nextLine();
+        JSONObject request = new JSONObject();
+        request.put("method", "join");
+        request.put("username", username);
+        send(request.toString());
+        try {
+            String input = in.readUTF();
+            JSONObject response = new JSONObject(input);
+            String status = response.getString("status");
+            switch (status) {
+                case "ok" : 
+                    playerId = response.getInt("player_id");
+                    System.out.println("Your player id is " + playerId + ".");
+                    break;
+                case "fail" :
+                    System.out.println("Failed, " + response.getString("description") + ".");
+                    break;
+                case "error" :
+                    System.out.println("Failed, " + response.getString("description") + ".");
+                    break;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+    }
+    
+    private void leaveCommand() {
+        JSONObject request = new JSONObject();
+        request.put("method", "leave");
+        send(request.toString());
+        try {
+            String input = in.readUTF();
+            JSONObject response = new JSONObject(input);
+            String status = response.getString("status");
+            switch (status) {
+                case "ok" : 
+                    System.out.println("You have successfully leave the game.");
+                    break;
+                case "fail" :
+                    System.out.println("Failed, " + response.getString("description") + ".");
+                    break;
+                case "error" :
+                    System.out.println("Failed, " + response.getString("description") + ".");
+                    break;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void readyUpCommand() {
+        JSONObject request = new JSONObject();
+        request.put("method", "ready");
+        send(request.toString());
+        try {
+            String input = in.readUTF();
+            JSONObject response = new JSONObject(input);
+            String status = response.getString("status");
+            switch (status) {
+                case "ok" : 
+                    System.out.println(response.getString("description"));
+                    input = in.readUTF();
+                    System.out.println(input);
+                    startPlaying();
+                    break;
+                case "error" :
+                    System.out.println("Failed, " + response.getString("description") + ".");
+                    break;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    private void startPlaying() {
+        System.out.println("Playing...");
+        // Isi dengan perintah bermain
+    }
+    
+    private void listClientCommand() {
+        JSONObject request = new JSONObject();
+        request.put("method", "client_address");
+        send(request.toString());
+        try {
+            String input = in.readUTF();
+            JSONObject response = new JSONObject(input);
+            String status = response.getString("status");
+            switch (status) {
+                case "ok" : 
+                    System.out.println(response.get("clients"));
+                    break;
+                case "fail" :
+                    System.out.println("Failed, " + response.getString("description") + ".");
+                    break;
+                case "error" :
+                    System.out.println("Failed, " + response.getString("description") + ".");
+                    break;
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String args[]) {
+        String ipAddr = "127.0.0.1";
+        int port = 8080;
+        Client client = new Client(ipAddr, port);
+        System.out.println("Connected to " + ipAddr + " port " + port + "...");
+        client.run();
     }
 }
